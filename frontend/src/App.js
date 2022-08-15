fd84ea1b539dfbe9b6b41d05a44e87d38d8dba5f
serjabal <serjabal@espol.edu.ec>
2022-08-14 19:31:44 -0500

    Se agregó la navigation.js y se ha configurado la vista de la página web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
+//import Show from './components/ShowEstudiantes'
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Navigation/>
+     <Routes>
+       <Route path="/topArea" element={<div>AQUI DEBE IR EL TOP AREA</div>} />
+       <Route path="/topClientes" element={<div>AQUI DEBE IR EL GRAFICO ESTADISTICO</div>} />
+       <Route path="/topVentas" element={<div>AQUI DEBE IR EL TOP DE VENTAS</div>} />
+
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
